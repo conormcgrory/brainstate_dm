9cf3a342fdb2c99d6a536a736245f7e2d112e434
Conor McGrory <cpmcgrory@gmail.com>
2021-11-20 20:31:17 -0500

    Wrote IBLTask class

--- notebooks/tasks.py
+++ notebooks/tasks.py
@@ -7,7 +7,7 @@ from numpy.random import default_rng
 
 class GlazeTask:
 
-    def __init__(self, hazard_rate, noise, s_init=1, rng=default_rng()):
+    def __init__(self, hazard_rate, noise, rng=default_rng()):
 
         # Set parameters
         self.hazard_rate = hazard_rate
@@ -15,7 +15,21 @@ class GlazeTask:
         self.rng = rng
 
         # Initialize side
-        self.s_current = s_init
+        self.s_current = rng.choice([-1, 1])
+
+    def _smp_twopoint(self, p):
+
+        return self.rng.choice([1, -1], p=[p, 1 - p])
+
+    def next_trial(self):
+
+        # Side switches with probability equal to hazard rate
+        self.s_current *= -self._smp_twopoint(self.hazard_rate)
+
+        # Observation is side plus Gaussian noise
+        x = self.s_current + self.rng.normal(0, self.noise)
+
+        return (self.s_current, x)
 
     def sample_trials(self, n_trials):
 
@@ -23,48 +37,48 @@ class GlazeTask:
         x_vals = np.full(n_trials, np.nan)
 
         for i in range(n_trials):
+            s_vals[i], x_vals[i] = self.next_trial()
         
-            # Add current side and observation to arrays
-            s_vals[i] = self.s_current
-            x_vals[i] = self.s_current + self.rng.normal(0, self.noise)
-        
-            # Side switches with probability equal to hazard rate
-            if self.rng.binomial(1, self.hazard_rate):
-                self.s_current = -self.s_current
-
         return s_vals, x_vals
  
 
-#def gen_glaze_trials(n_smps, rng, hazard_rate=0.1, noise=0.5, s_init=0):
-#   
-#    side = s_init
-#    
-#    for i in range(n_smps):
-#        
-#        x = side + rng.normal(0, noise)
-#        
-#        yield (side, x)
-#        
-#        # Switch sides with probability equal to hazard rate
-#        if rng.binomial(1, hazard_rate):
-#            side = -side
-            
-            
-def gen_ibl_trials(n_smps, rng, hazard_rate=0.1, p_right=[0.2, 0.8], c_vals=[12, 25, 50, 100], b_init=0):
-   
-    block = b_init
-    
-    for i in range(n_smps):
+class IBLTask:
+
+    def __init__(self, hazard_rate, noise, alpha=0.8, rng=default_rng()):
+
+        # Set parameters
+        self.hazard_rate = hazard_rate
+        self.noise = noise
+        self.alpha = alpha
+        self.rng = rng
+
+        # Initialize block
+        self.b_current = rng.choice([-1, 1])
+
+    def _smp_twopoint(self, p):
+
+        return self.rng.choice([1, -1], p=[p, 1-p])
+
+    def next_trial(self):
+
+        # Block switches with probability equal to hazard rate
+        self.b_current *= -self._smp_twopoint(self.hazard_rate) 
+
+        # Side probability depends on block
+        s = self._smp_twopoint(self.alpha) * self.b_current
+
+        # Observation is side plus Gaussian noise
+        x = s + self.rng.normal(0, self.noise)
+
+        return (self.b_current, s, x)
+
+    def sample_trials(self, n_trials):
+
+        b_vals = np.full(n_trials, np.nan)
+        s_vals = np.full(n_trials, np.nan)
+        x_vals = np.full(n_trials, np.nan)
+
+        for i in range(n_trials):
+            b_vals[i], s_vals[i], x_vals[i] = self.next_trial()
         
-        # Sample side (L=-1, R=1) using probability for current block value
-        side = 1 - 2 * rng.binomial(1, p_right[block])
-        
-        # Sample contrast uniformly from c_vals, with sign determined by side
-        signed_contrast = side * rng.choice(c_vals) 
-        
-        yield (block, side, signed_contrast)
-        
-        # Switch blocks with Probability equal to hazard rate
-        if rng.binomial(1, hazard_rate):
-            block = 1 - block
- 
+        return b_vals, s_vals, x_vals
